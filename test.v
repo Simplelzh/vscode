2ccdfb0ddee3fe4b82e94649ca5ed1ac5421071d
Simplelzh <578011884@qq.com>
2022-06-08 13:48:27 +0800

    test

--- test.v
+++ test.v
@@ -1,3 +1,11 @@
 module test;
-sss
+    input   ss;
+    output   ss1;
+    
+    //嘻嘻
+    always @(posedge clk or negedge rstn) begin
+        if (rstn)begin
+            ss1 <= 'd0;
+        end
+    end
 endmodule
